d006363de7fe1e557b853470b8347b78a588a4bd
Abdallah Ramsing <yashircr@gmail.com>
2023-04-03 15:26:43 +0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const getUsers = axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const getPosts = axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        const getComments = axios.get(
+          "https://jsonplaceholder.typicode.com/comments"
+        );
+        const [usersResponse, postsResponse, commentsResponse] =
+          await Promise.all([getUsers, getPosts, getComments]);
+        setUsers(usersResponse.data);
+        setPosts(postsResponse.data);
+        setComments(commentsResponse.data);
+
+        mapData(users, posts, comments);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [comments, posts, users]);
+
+  const mapData = (users, posts, comments) => {
+    const mapped = posts.map((p) => {
+      const post = p;
+      post.comments = comments.filter((c) => c.postId === p.id);
+      post.user = users.find((u) => u.id === post.userId);
+      return post;
+    });
+    setData(mapped);
+  };
+
+  return <div className="App">{JSON.stringify(data)}</div>;
 }
 
 export default App;
